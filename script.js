00665f42f1ec6a839a2c37c034b5a32cff514fd3
Swaraj Kashyap <swarajkumar7011@gmail.com>
2025-03-30 12:31:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true, 
+    grabCursor: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    }
+  });
